3e6b02b826126dde36abec145bf41dd93b71d284
menomenonkwan <menomenonkwan@gmail.com>
2021-10-25 16:20:19 -0700

    first commit

---
+++ templates/footer.php
@@ -0,0 +1,10 @@
+
+  <footer class="section">
+    <div class="center grey-text">Copyright <span class="copyright"></span> Brannon Lee</div>
+  </footer>
+
+  <script>
+    const today = new Date();
+    document.querySelector('.copyright').textContent = today.getFullYear();
+  </script>
+</body>
